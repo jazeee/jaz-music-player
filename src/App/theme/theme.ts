02f9c906839533cbc926dd51ee5dc313d6ae41cd
Jaz Singh <jaz@color.com>
2020-12-24 19:39:08 -0800

    Add category highlighting

--- src/App/theme/theme.ts
+++ src/App/theme/theme.ts
@@ -1,14 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { purple, orange } from '@material-ui/core/colors';
+import { purple, yellow } from '@material-ui/core/colors';
 
 export const theme = createMuiTheme({
   palette: {
     mode: 'dark',
-    primary: {
-      main: purple[500],
-    },
-    secondary: {
-      main: orange[500],
-    },
+    primary: purple,
+    secondary: yellow,
   },
 });
--- src/Music/CategoryResults/CategoryResults.tsx
+++ src/Music/CategoryResults/CategoryResults.tsx
@@ -1,4 +1,5 @@
-import { Table, TableContainer, TableBody, TableRow, TableCell, Paper, styled } from "@material-ui/core";
+import { Table, TableContainer, TableBody, TableRow, TableCell, Paper, styled, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import { CategoryType } from "../data/categories";
 import { ALL_BY_CATEGORY, ALL_INDICES, MUSIC_BY_CATEGORY } from "../data/data"
 import { useSelectedMusicContext } from "../SelectedMusic";
@@ -11,26 +12,53 @@ interface Props {
   category: CategoryType;
 }
 
+const ALL_KEY = 'ALL_OF_THEM!!!####UNIQUE####';
+
 export function CategoryResults(props: Props) {
   const { category } = props;
   const allByCategory = ALL_BY_CATEGORY[category];
   const { setSelectedIndices } = useSelectedMusicContext();
   const musicByCategory = MUSIC_BY_CATEGORY[category];
+  const [selectedItem, setSelectedItem] = useState<string>();
+  useEffect(() => {
+    if (category) {
+      setSelectedItem(ALL_KEY);
+      setSelectedIndices(new Set(ALL_INDICES));
+    }
+  }, [category, setSelectedIndices]);
+
+  function onClick(item: string) {
+    setSelectedItem(item);
+    if (item === ALL_KEY) {
+      setSelectedIndices(new Set(ALL_INDICES));
+    } else {
+      setSelectedIndices(new Set(musicByCategory[item]));
+    }
+  };
+
   return (
     <TableContainer component={Paper}>
       <Table aria-label="simple table">
         <TableBody>
-          <SelectableTableRow onClick={() => { setSelectedIndices(new Set(ALL_INDICES))}}>
-            <TableCell>All</TableCell>
-          </SelectableTableRow>
-          {allByCategory.map((item) => (
-            <SelectableTableRow key={item} onClick={() => { setSelectedIndices(new Set(musicByCategory[item]))}}>
-              <TableCell>
-                {item}
-              </TableCell>
-            </SelectableTableRow>
-          ))}
+          {[ALL_KEY, ...allByCategory].map((item) => {
+            let text = item;
+            if (item === ALL_KEY) {
+              text = 'All';
+            }
+            const isSelected = selectedItem === item;
+            return (
+              <SelectableTableRow key={item} onClick={() => {onClick(item);}} selected={isSelected}>
+                <TableCell>
+                  <Typography variant={isSelected ? "h5" : "body2"}>
+                    {isSelected && <span>&gt; </span>}
+                    {text}
+                  </Typography>
+                </TableCell>
+              </SelectableTableRow>
+            );
+          })}
         </TableBody>
       </Table>
-    </TableContainer>  )
+    </TableContainer>
+  );
 }
